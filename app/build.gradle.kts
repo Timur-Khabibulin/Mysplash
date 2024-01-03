5018a8d9a0309191a2d889f6609ec3dc052f4e03
Timur Khabibulin <101925716+Timur-Khabibulin@users.noreply.github.com>
2024-01-03 23:01:43 +0500

    Increment app version.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -31,8 +31,8 @@ android {
         applicationId = "com.timurkhabibulin.mysplash"
         minSdk = 26
         targetSdk = 34
-        versionCode = 1
-        versionName = "1.0-beta"
+        versionCode = 2
+        versionName = "1.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
